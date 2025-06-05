a044b5b8dfe8e81a2d9d1bfbb01572c262f18f1f
Matheus Araujo <mun0t0@duck.com>
2025-06-05 20:01:22 -0300

    feat: added URLschemas

---
+++ src/domain/entities/url/schemas/index.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export class URLSchema {
+  public static readonly originalUrl = z.string().url();
+  public static readonly shortId = z.string().length(6);
+  // public static readonly accessCounter = z.coerce.number().min(1);
+
+  public static readonly createUrl = z.object({
+    originalUrl: this.originalUrl,
+  });
+
+  public static readonly urlEntity = z.object({
+    originalUrl: this.originalUrl,
+    shortId: this.shortId,
+    // accessCounter: this.accessCounter,
+  });
+}
